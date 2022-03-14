54074cb8c7244428dffa725ed3f9e529fd27cbc0
John <ekapob.t@ku.th>
2022-03-14 19:44:51 +0700

    enemy turnwhen target in vision

--- 3D Action RPG/3D Action RPG/Assets/Scripts/Unit/Enemy/Enemy.cs
+++ 3D Action RPG/3D Action RPG/Assets/Scripts/Unit/Enemy/Enemy.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour
+public class Enemy : UnitBase
 {
     private Transform target;
     private Vector3 startPosition;
@@ -10,8 +10,11 @@ public class Enemy : MonoBehaviour
     private float offsetRange = 10f;
     private bool _isOutOfSafeArea = false;
 
-    private void Awake()
+    public float turnValue = 0.5f;
+
+    protected override void Awake()
     {
+        base.Awake();
         startPosition = transform.localPosition;
     }
 
@@ -20,32 +23,46 @@ public class Enemy : MonoBehaviour
     {
         // Set X and Y to move
         if (target != null)
-        {
-
-        }
+            MoveToTarget();
         else
             StayInSafeArea();
     }
 
     public void SetTarget(Transform t) => target = t;
 
-    private void MoveTo()
+    private void MoveToTarget()
     {
+        Vector3 localPos = transform.InverseTransformPoint(target.transform.position);
+
+        //transform.rotation = Quaternion.Lerp(transform.rotation, rotationAngle, Time.deltaTime * 2f); 
+
+        if (localPos.x < -0.15f) // left
+           SetInputVectorX(-turnValue);
+        else if (localPos.x > 0.15f) // right
+            SetInputVectorX(turnValue);
+        else // center
+        {
+            SetInputVectorX(0);
+        }
+        //Debug.Log($"MoveToTarget {angle} {angleAxis}");
         // move to target if move out of spawn range back to original poistion
+        if (IsOutOfAreaOffset())
+            SetTarget(null);
     }
     
     private void StayInSafeArea()
     {
         if (!_isOutOfSafeArea)
             return;
+        // move to startPosition
         // _isOutOfSafeArea = true; // after back to default path
     }
 
     private bool IsOutOfAreaOffset()
     {
         var sumPosition = transform.localPosition - startPosition;
-        var currentPosition = Mathf.Abs(sumPosition.x) + Mathf.Abs(sumPosition.z);
+        var sumPositionResult = Mathf.Abs(sumPosition.x) + Mathf.Abs(sumPosition.z);
 
-        return currentPosition > offsetRange;
+        return sumPositionResult > offsetRange;
     }
 }
